e7b40ff07b78a7f1a180587c5f0b6b81d64a267f
home <ibasha36@gmail.com>
2022-07-03 18:41:20 +0530

    screen design

--- eCommerce/eCom/urls.py
+++ eCommerce/eCom/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
 ]
--- eCommerce/eCom/views.py
+++ eCommerce/eCom/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def register(request):
+    return render(request,"register.html")
+def index(request):
     return render(request,"index.html")
+def login(request):
+    return render(request,"login.html")
